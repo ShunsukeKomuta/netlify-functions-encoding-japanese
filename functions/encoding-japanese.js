6aefff47508a240a84708ac5bca14529b415a200
Shunsuke Komuta <shunsuke.komuta@gmail.com>
2022-07-29 15:52:04 +0900

    Update encoding-japanese.js

--- functions/encoding-japanese.js
+++ functions/encoding-japanese.js
@@ -21,6 +21,11 @@ exports.handler = async function (event, context, callback) {
         .replace("charset=Shift_JIS", "charset=UTF-8")
         .replace('<?xml version="1.0" encoding="shift_jis"?>', '');
       break;
+    case "EUCJP"
+      replaced = utf8text
+        .replace("charset=EUC-JP", "charset=UTF-8")
+        .replace('<?xml version="1.0" encoding="euc_jp"?>', '');
+      break;
     default:
       break;
   }
